4c721cfd658bbb61d4e0b14d7606e67a1e3881b7
kkr970 <58022426+kkr970@users.noreply.github.com>
2022-02-23 11:55:44 +0900

    Day55
    -Collision Resolution
    -repositioning
    -change direction
    
    -game reset
    -next level

--- src/19_game.h
+++ src/19_game.h
@@ -4,6 +4,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <vector>
+#include <tuple>
 
 #include "19_sprite_renderer.h"
 #include "19_resource_manager.h"
@@ -16,16 +17,48 @@ enum GameState {
     GAME_MENU,
     GAME_WIN
 };
+//방향
+enum Direction {
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT
+};
 
 //player paddle 설정
-const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
-const float PLAYER_VELOCITY(500.0f);
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f); //크기
+const float PLAYER_VELOCITY(500.0f);  //속도
 
 //ball 설정
-const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f); //속도
 //const glm::vec2 INITIAL_BALL_VELOCITY(10.0f, -35.0f);
-const float BALL_RADIUS = 12.5f;
+const float BALL_RADIUS = 12.5f; //크기, 반지름
 
+//충돌데이터 튜플
+typedef std::tuple<bool, Direction, glm::vec2> Collision;
+
+//충돌 방향 벡터 구하기
+Direction VectorDirection(glm::vec2 target)
+{
+    glm::vec2 compass[] = {
+        glm::vec2(0.0f, 1.0f),	// up
+        glm::vec2(1.0f, 0.0f),	// right
+        glm::vec2(0.0f, -1.0f),	// down
+        glm::vec2(-1.0f, 0.0f)	// left
+    };
+    float max = 0.0f;
+    unsigned int best_match = -1;
+    for (unsigned int i = 0; i < 4; i++)
+    {
+        float dot_product = glm::dot(glm::normalize(target), compass[i]);
+        if (dot_product > max)
+        {
+            max = dot_product;
+            best_match = i;
+        }
+    }
+    return (Direction)best_match;
+} 
 //충돌 함수
 bool CheckCollision(GameObject &one, GameObject &two) //AABB-AABB
 {
@@ -38,7 +71,7 @@ bool CheckCollision(GameObject &one, GameObject &two) //AABB-AABB
     //x, y둘다 감지되면 충돌
     return collisionX && collisionY;
 } 
-bool CheckCollision(BallObject &one, GameObject &two) // AABB-Circle
+Collision CheckCollision(BallObject &one, GameObject &two) // AABB-Circle
 {
     // get center point circle first 
     glm::vec2 center(one.Position + one.Radius);
@@ -55,14 +88,16 @@ bool CheckCollision(BallObject &one, GameObject &two) // AABB-Circle
     glm::vec2 closest = aabb_center + clamped;
     // retrieve vector between center circle and closest point AABB and check if length <= radius
     difference = closest - center;
-    return glm::length(difference) < one.Radius;
+    if (glm::length(difference) <= one.Radius)
+        return std::make_tuple(true, VectorDirection(difference), difference);
+    else
+        return std::make_tuple(false, UP, glm::vec2(0.0f, 0.0f));
 }     
 
 //게임 클래스
 class Game
 {
 private:
-    //스프라이트 렌더러
     SpriteRenderer *Renderer;
     GameObject *Player;
     BallObject *Ball;
@@ -154,31 +189,106 @@ public:
                 Ball->Stuck = false;
         }
     }
-
+    // game reset
+    void ResetLevel()
+    {
+        if (this->Level == 0)
+            this->Levels[0].Load("resources/gamelevel/1.txt", this->Width, this->Height / 2);
+        else if (this->Level == 1)
+            this->Levels[1].Load("resources/gamelevel/2.txt", this->Width, this->Height / 2);
+        else if (this->Level == 2)
+            this->Levels[2].Load("resources/gamelevel/3.txt", this->Width, this->Height / 2);
+        else if (this->Level == 3)
+            this->Levels[3].Load("resources/gamelevel/4.txt", this->Width, this->Height / 2);
+    }
+    // player reset
+    void ResetPlayer()
+    {
+        // reset player/ball stats
+        Player->Size = PLAYER_SIZE;
+        Player->Position = glm::vec2(this->Width / 2.0f - PLAYER_SIZE.x / 2.0f, this->Height - PLAYER_SIZE.y);
+        Ball->Reset(Player->Position + glm::vec2(PLAYER_SIZE.x / 2.0f - BALL_RADIUS, -(BALL_RADIUS * 2.0f)), INITIAL_BALL_VELOCITY);
+}
+    // next level
+    void NextLevel()
+    {
+        this->Level++;
+        ResetLevel();
+        ResetPlayer();
+    }
+    // 충돌 처리
     void DoCollisions()
     {
         for (GameObject &box : this->Levels[this->Level].Bricks)
         {
             if (!box.Destroyed)
             {
-                if (CheckCollision(*Ball, box))
+                Collision collision = CheckCollision(*Ball, box);
+                if (std::get<0>(collision)) // if collision is true
                 {
+                    // destroy block if not solid
                     if (!box.IsSolid)
                         box.Destroyed = true;
+                    // collision resolution
+                    Direction dir = std::get<1>(collision);
+                    glm::vec2 diff_vector = std::get<2>(collision);
+                    if (dir == LEFT || dir == RIGHT) // horizontal collision
+                    {
+                        Ball->Velocity.x = -Ball->Velocity.x; // reverse horizontal velocity
+                        // relocate
+                        float penetration = Ball->Radius - std::abs(diff_vector.x);
+                        if (dir == LEFT)
+                            Ball->Position.x += penetration; // move ball to right
+                        else
+                            Ball->Position.x -= penetration; // move ball to left;
+                    }
+                    else // vertical collision
+                    {
+                        Ball->Velocity.y = -Ball->Velocity.y; // reverse vertical velocity
+                        // relocate
+                        float penetration = Ball->Radius - std::abs(diff_vector.y);
+                        if (dir == UP)
+                            Ball->Position.y -= penetration; // move ball back up
+                        else
+                            Ball->Position.y += penetration; // move ball back down
+                    }
                 }
             }
         }
-    } 
+        Collision result = CheckCollision(*Ball, *Player);
+        if (!Ball->Stuck && std::get<0>(result))
+        {
+            // check where it hit the board, and change velocity based on where it hit the board
+            float centerBoard = Player->Position.x + Player->Size.x / 2.0f;
+            float distance = (Ball->Position.x + Ball->Radius) - centerBoard;
+            float percentage = distance / (Player->Size.x / 2.0f);
+            // then move accordingly
+            float strength = 2.0f;
+            glm::vec2 oldVelocity = Ball->Velocity;
+            Ball->Velocity.x = INITIAL_BALL_VELOCITY.x * percentage * strength; 
+            Ball->Velocity.y = -1.0f * abs(Ball->Velocity.y);  
+            Ball->Velocity = glm::normalize(Ball->Velocity) * glm::length(oldVelocity);
+        } 
+    }   
 
+    //게임 상황 업데이트
     void Update(float dt)
     {
         // 오브젝트 업데이트
         Ball->Move(dt, this->Width);
-
         // 충돌 감지
         this->DoCollisions();
+        // 게임 오버
+        if (Ball->Position.y >= this->Height)
+        {
+            this->ResetLevel();
+            this->ResetPlayer();
+        }
+        // 레벨 클리어, 다음 레벨 진행
+        if(this->Levels[this->Level].IsCompleted())
+            NextLevel();
     }
-
+    //게임 렌더링
     void Render()
     {
         if(this->State == GAME_ACTIVE)
